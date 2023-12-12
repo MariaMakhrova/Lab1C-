ca6262a2daca7323ca94d49710ff3e590f13336c
MariaMakhrova <148859148+MariaMakhrova@users.noreply.github.com>
2023-12-13 01:06:20 +0200

    Add files via upload

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+class Program
+{
+	static async Task Main()
+	{
+		string connectionString = "Data Source=(local);Initial Catalog=<назва_групи>_<прізвище_студента>;Integrated Security=True";
+
+		using (var dbContext = new YourDbContext(connectionString))
+		{
+			// a) Простий асинхронний запит на вибірку
+			var resultA = await dbContext.Гуртожиток.ToListAsync();
+			DisplayResults(resultA);
+
+			// b) Асинхронний запит на вибірку з використанням спеціальних функцій
+			Console.Write("Введіть значення для параметра корпус: ");
+			string корпусValueB = Console.ReadLine();
+			var resultB = await dbContext.Гуртожиток
+				.Where(g => EF.Functions.Like(g.Корпус, $"{корпусValueB}%"))
+				.ToListAsync();
+			DisplayResults(resultB);
+
+			// c) Асинхронний запит зі складним критерієм
+			Console.Write("Введіть значення для параметра оплата: ");
+			int оплатаValueC = int.Parse(Console.ReadLine());
+			Console.Write("Введіть значення для параметра умови: ");
+			string умовиValueC = Console.ReadLine();
+			var resultC = await dbContext.Гуртожиток
+				.Where(g => g.Оплата > оплатаValueC && g.Умови == умовиValueC)
+				.ToListAsync();
+			DisplayResults(resultC);
+		}
+	}
+
+	static void DisplayResults(List<Гуртожиток> results)
+	{
+		foreach (var item in results)
+		{
+			Console.WriteLine($"{item.Корпус}, {item.Кімнати}, {item.Студенти}, {item.Оплата}, {item.Умови}");
+		}
+		Console.WriteLine();
+	}
+}
+
+public class YourDbContext : DbContext
+{
+	public YourDbContext(string connectionString) : base(new DbContextOptionsBuilder<YourDbContext>().UseSqlServer(connectionString).Options)
+	{
+	}
+
+	public DbSet<Гуртожиток> Гуртожиток { get; set; }
+}
+
+public class Гуртожиток
+{
+	public string Корпус { get; set; }
+	public int Кімнати { get; set; }
+	public int Студенти { get; set; }
+	public int Оплата { get; set; }
+	public string Умови { get; set; }
+}
