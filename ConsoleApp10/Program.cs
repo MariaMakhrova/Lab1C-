c3c9315cec59b4bd54b58d5fb591f97c91f2d984
MariaMakhrova <148859148+MariaMakhrova@users.noreply.github.com>
2023-12-20 00:32:32 +0200

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Виберіть спосіб авторизації:");
+		Console.WriteLine("1. Логін і пароль");
+		Console.WriteLine("2. Авторизація кодом із СМС");
+		Console.WriteLine("3. Авторизація за допомогою зовнішнього серверу");
+
+		string choice = Console.ReadLine();
+
+		switch (choice)
+		{
+			case "1":
+				if (LoginWithUsernameAndPassword())
+				{
+					Console.WriteLine("Привіт!");
+				}
+				else
+				{
+					Console.WriteLine("Невірний логін або пароль.");
+				}
+				break;
+
+			case "2":
+				if (LoginWithSMSCode())
+				{
+					Console.WriteLine("Привіт!");
+				}
+				else
+				{
+					Console.WriteLine("Невірний код із СМС.");
+				}
+				break;
+
+			case "3":
+				if (LoginWithExternalServer())
+				{
+					Console.WriteLine("Привіт!");
+				}
+				else
+				{
+					Console.WriteLine("Помилка авторизації на зовнішньому сервері.");
+				}
+				break;
+
+			default:
+				Console.WriteLine("Невірний вибір способу авторизації.");
+				break;
+		}
+	}
+
+	static bool LoginWithUsernameAndPassword()
+	{
+		Console.Write("Введіть логін: ");
+		string enteredUsername = Console.ReadLine();
+
+		Console.Write("Введіть пароль: ");
+		string enteredPassword = Console.ReadLine();
+
+		return enteredUsername == "example_user" && enteredPassword == "example_password";
+	}
+
+	static bool LoginWithSMSCode()
+	{
+		string sentCode = "123456";
+
+		Console.Write("Введіть код із СМС: ");
+		string enteredCode = Console.ReadLine();
+
+		return enteredCode == sentCode;
+	}
+
+	static bool LoginWithExternalServer()
+	{
+		Console.Write("Виберіть сервіс (Гугл, Фейсбук чи Дія): ");
+		string selectedService = Console.ReadLine();
+
+		Console.Write("Введіть логін: ");
+		string enteredUsername = Console.ReadLine();
+
+		return true;
+	}
+}
